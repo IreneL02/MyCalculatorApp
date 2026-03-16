90a9d7e30eee571cfa984d74977ef1713adf7d9d
Irene <accforgamesfrom2021@gmail.com>
2026-03-16 13:57:12 +0200

    Add files via upload

---
+++ MyCalculatorApp/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCalculatorApp
+{
+    public class Calculator{
+       public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ MyCalculatorApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using MyCalculatorApp;
+
+Calculator calc = new Calculator();
+Console.WriteLine($"The sum is: {calc.Sum(5, 3)}");
